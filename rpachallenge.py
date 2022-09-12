df266fc58d4c90a3933603ad07b01b11a98c8246
SamaelPicoli <76179103+Samaelpicoli@users.noreply.github.com>
2022-09-12 16:04:53 -0300

    Update rpachallenge.py

--- rpachallenge.py
+++ rpachallenge.py
@@ -3,8 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
-from selenium.webdriver.common.alert import Alert
 
 #lendo o arquivo xlsx
 arquivo = 'challenge.xlsx'
